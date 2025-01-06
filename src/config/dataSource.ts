07b71b78b3f40a8002079c85a8b49d17fe0fd661
undead04 <antran.261004@gmail.com>
2025-01-06 22:42:21 +0700

    configdeloy

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,10 +1,9 @@
-
 import { DataSource } from "typeorm";
 import { RedisCache } from "./redisCache";
 const redisConfig = {
-  host: 'localhost',  // Địa chỉ Redis server
-  port: Number(process.env.PORT_REDIS)||6379,         // Cổng mặc định của Redis
-  db: 0,              // Sử dụng DB 0 trong Redis
+  host: "localhost", // Địa chỉ Redis server
+  port: Number(process.env.PORT_REDIS) || 6379, // Cổng mặc định của Redis
+  db: 0, // Sử dụng DB 0 trong Redis
   retryStrategy: (times) => {
     // Nếu Redis mất kết nối, thử kết nối lại sau 1 giây
     return Math.min(times * 50, 2000);
@@ -15,25 +14,22 @@ export const redisCache = new RedisCache(redisConfig);
 export const dataSource = new DataSource({
   type: "mysql",
   host: "localhost",
-  port: 3306,
-  username: "root",
-  database: process.env.DATABASE||'food_suggestion',
+  port: Number(process.env.PORT_MYSQL) || 3306,
+  username: process.env.USERSQL || "root",
+  database: process.env.DATABASE || "food_suggestion",
   synchronize: false,
   logging: true,
   entities: ["src/entitys/*.ts"],
   migrations: [
     // Dẫn đến thư mục migrations của bạn
-    "src/migrations/*.ts"
+    "src/migrations/*.ts",
   ],
-  subscribers:[],
+  subscribers: [],
   cache: {
-    type: "redis",       // Sử dụng Redis làm cache
-    duration: 10000,     // Thời gian sống của cache (tính bằng milisecond)
+    type: "redis", // Sử dụng Redis làm cache
+    duration: 10000, // Thời gian sống của cache (tính bằng milisecond)
     options: {
-      client: redisCache,  // Kết nối Redis client
+      client: redisCache, // Kết nối Redis client
     },
   },
-
-  
 });
-
